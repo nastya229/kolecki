eed5f4789cfb2ed5db3f4b6961f09595e1554412
nastya229 <zmix23@mail.ru>
2026-04-21 19:40:32 +0700

    disable whitenoise compression

--- kolecki/kolecki/settings.py
+++ kolecki/kolecki/settings.py
@@ -73,7 +73,7 @@ USE_TZ = True
 STATIC_URL = "/static/"
 STATIC_ROOT = BASE_DIR / "staticfiles"
 STATICFILES_DIRS = [BASE_DIR / "main/static"]
-STATICFILES_STORAGE = "whitenoise.storage.CompressedStaticFilesStorage"
+STATICFILES_STORAGE = "django.contrib.staticfiles.storage.StaticFilesStorage"
 
 MEDIA_URL = "/media/"
 MEDIA_ROOT = BASE_DIR / "media"
